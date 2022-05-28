9783d73dc925919d6373c7469d569f4621046d3f
Arthur Luz <arthurmluz123@gmail.com>
2022-05-28 15:50:18 -0300

    content com request funcionando (menos impressao)

--- content.js
+++ content.js
@@ -4,10 +4,33 @@ var selectedText;
 
 function handleSelection() {
   selectedText = window.getSelection().toString().replace(/\s/g, "");
+  pf();
+
 }
 
 chrome.runtime.onMessage.addListener(gotMessage);
 
+async function pf(){
+  console.log("oi");
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  var raw = JSON.stringify({
+    "txt": "algum ser humano desgraçado sendo a mão no interfone aqui de casa! FDP"
+  });
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://100tistas.duckdns.org/", requestOptions)
+    .then(response => console.log(response))
+    //.then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
 function gotMessage(message, sender, sendResponse) {
   let msg =
     selectedText && selectedText.length > 0
